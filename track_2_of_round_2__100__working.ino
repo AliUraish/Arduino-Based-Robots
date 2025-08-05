717827d23be176f75803521ae0cdd2507ca02c4c
Ali Uraish <aliuraishmirani@gmail.com>
2025-08-05 13:27:00 +0500

    Final round 2 working.

---
+++ track_2_of_round_2__100__working.ino
@@ -0,0 +1,163 @@
+int ena1=5;
+int ena2=6;
+int in1=7;
+int in2=8;
+int in3=9;
+int in4=10;
+int Lsensor=13;
+int Msensor1=12;
+int Msensor2=11;
+int Rsensor=4;
+int LS,MS1,MS2,RS;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ena1,OUTPUT);
+pinMode(ena2,OUTPUT);
+pinMode(in1,OUTPUT);
+pinMode(in2,OUTPUT);
+pinMode(in3,OUTPUT);
+pinMode(in4,OUTPUT);
+pinMode(Lsensor,INPUT);
+pinMode(Msensor1,INPUT);
+pinMode(Msensor2,INPUT);
+pinMode(Rsensor,INPUT);
+
+Serial.begin(9600);
+
+}
+
+void loop() {
+  LS=digitalRead(Lsensor);
+  MS1=digitalRead(Msensor1);
+  MS2=digitalRead(Msensor2);
+  RS=digitalRead(Rsensor);
+  Serial.print(LS);
+  Serial.print("\t");
+  Serial.print(MS1);
+  Serial.print("\t");
+  Serial.print(MS2);
+  Serial.print("\t");
+  Serial.println(RS);
+  
+    analogWrite(ena1,100);
+    digitalWrite(in1,0);
+    digitalWrite(in2,1);
+    analogWrite(ena2,100);
+    digitalWrite(in3,0);
+    digitalWrite(in4,1);
+  if (LS==0 && MS1==0 && MS2==0 && RS==0 )
+  {
+    analogWrite(ena1,80);
+    digitalWrite(in1,0);
+    digitalWrite(in2,1);
+    analogWrite(ena2,80);
+    digitalWrite(in3,0);
+    digitalWrite(in4,1);
+    
+    //Stop
+    }
+   else if(LS==0 && MS1==0 && MS2==0 && RS==1)
+   {
+    analogWrite(ena1,160);
+    digitalWrite(in1,1);
+    digitalWrite(in2,0);
+    analogWrite(ena2,160);
+    digitalWrite(in3,1);
+    digitalWrite(in4,0);
+    //Sharp Right
+    }
+    else if(LS==0 && MS1==0 && MS2==1 && RS==1)
+   {
+    analogWrite(ena1,120);
+    digitalWrite(in1,1);
+    digitalWrite(in2,0);
+    analogWrite(ena2,120);
+    digitalWrite(in3,0);
+    digitalWrite(in4,1);
+    //Right Soft
+    }
+    else if(LS==0 && MS1==1 && MS2==1 && RS==0)
+   {
+    analogWrite(ena1,100);
+    digitalWrite(in1,0);
+    digitalWrite(in2,1);
+    analogWrite(ena2,100);
+    digitalWrite(in3,0);
+    digitalWrite(in4,1);
+    //Straight
+    }
+//
+     else if(LS==0 && MS1==0 && MS2==1 && RS==0)
+   {
+    analogWrite(ena1,100);
+    digitalWrite(in1,0);
+    digitalWrite(in2,1);
+    analogWrite(ena2,100);
+    digitalWrite(in3,0);
+    digitalWrite(in4,1);
+    //Straight
+    }
+//
+     else if(LS==0 && MS1==1 && MS2==0 && RS==0)
+   {
+    analogWrite(ena1,100);
+    digitalWrite(in1,0);
+    digitalWrite(in2,1);
+    analogWrite(ena2,100);
+    digitalWrite(in3,0);
+    digitalWrite(in4,1);
+    //Straight
+    }
+//    
+    else if(LS==0 && MS1==1 && MS2==1 && RS==1)
+   {
+    analogWrite(ena1,170);
+    digitalWrite(in1,1);
+    digitalWrite(in2,0);
+    analogWrite(ena2,170);
+    digitalWrite(in3,1);
+    digitalWrite(in4,0);
+    // 70 Right
+    }
+    else if(LS==1 && MS1==0 && MS2==0 && RS==0)
+   {
+    analogWrite(ena1,160);
+    digitalWrite(in1,1);
+    digitalWrite(in2,0);
+    analogWrite(ena2,160);
+    digitalWrite(in3,1);
+    digitalWrite(in4,0);
+    //Sharp Left
+    }
+    else if(LS==1 && MS1==1 && MS2==0 && RS==0)
+   {
+    analogWrite(ena1,120);
+    digitalWrite(in1,0);
+    digitalWrite(in2,1);
+    analogWrite(ena2,120);
+    digitalWrite(in3,1);
+    digitalWrite(in4,0);
+    //Soft Left
+    }
+    else if(LS==1 && MS1==1 && MS2==1 && RS==0)
+   {  
+    analogWrite(ena1,170);
+    digitalWrite(in1,1);
+    digitalWrite(in2,0);
+    analogWrite(ena2,170);
+    digitalWrite(in3,1);
+    digitalWrite(in4,0);
+    //70 Left
+    }
+    else if(LS==1 && MS1==1 && MS2==1 && RS==1)
+   {
+    analogWrite(ena1,100);
+    digitalWrite(in1,0);
+    digitalWrite(in2,1);
+    analogWrite(ena2,100);
+    digitalWrite(in3,0);
+    digitalWrite(in4,1);
+    //Stright
+    }
+}
